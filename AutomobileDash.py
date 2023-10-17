a8edd7aa7c3acf2474233f0a1ff34f36c8adf1bb
LucasBandeira <lucastbandeira@gmail.com>
2023-10-17 14:07:17 -0300

    Recession report style improvement

--- AutomobileDash.py
+++ AutomobileDash.py
@@ -9,26 +9,24 @@ import plotly.express as px
 # Variables
 year_list = [i for i in range(1980, 2024, 1)]
 
-dropdown_options = [
-    {'label': '...........', 'value': 'Yearly Statistics'},
-    {'label': 'Recession Period Statistics', 'value': '.........'}
-]
+# Dark theme CSS
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 # Load the data using pandas
 data = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/historical_automobile_sales.csv')
 
-# Initialize the Dash app
-app = dash.Dash(__name__)
+# Initialize the Dash app with the dark theme
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # Set the title of the dashboard
 app.title = 'Automobile Statistics Dashboard'
 
 # Build dash app layout
-app.layout = html.Div([
-    html.H1('Automobile Sales Statistics Dashboard', style={'text-align': 'center', 'color': '#503D36', 'font-size': 24}),
-                       
+app.layout = html.Div(style={'backgroundColor': '#1E1E1E', 'color': 'white'}, children=[
+    html.H1('Automobile Sales Statistics Dashboard', style={'textAlign': 'center'}),
+    
     html.Div([
-        html.Label("Select Statistics:"),
+        html.Label("Select Statistics:", style={'padding': '3px', 'font-size': '20px', 'color': 'white'}),
         dcc.Dropdown(
             id='dropdown-statistics',
             options=[
@@ -37,18 +35,18 @@ app.layout = html.Div([
             ],
             value='Yearly Statistics',
             placeholder='Select a Report Type',
-            style={'width': '80%', 'padding': '3px', 'font-size': '20px', 'text-align-last': 'center'}
+            style={'padding': '3px', 'font-size': '20px', 'text-align-last': 'center', 'color': 'black'}
         )
-    ]),
+    ],style={'width': '50%'},),
     
     html.Div([
         dcc.Dropdown(
             id='select-year',
             options=[{'label': i, 'value': i} for i in year_list],
             placeholder='Select a Year',
-            style={'width': '80%', 'padding': '3px', 'font-size': '20px', 'text-align-last': 'center'}
+            style={'padding': '3px', 'font-size': '20px', 'text-align-last': 'center', 'color': 'black'}
         )
-    ]),
+    ],style={'width': '50%'},),
     
     html.Div([
         html.Div(id='output-container', className='chart-grid', style={'display': 'flex'}),
@@ -58,9 +56,8 @@ app.layout = html.Div([
 # Creating Callbacks
 @app.callback(
     Output(component_id='select-year', component_property='disabled'),
-    Input(component_id='dropdown-statistics', component_property='disabled')
-    )
-
+    Input(component_id='dropdown-statistics', component_property='value')
+)
 def update_input_container(selected_statistics):
     if selected_statistics == 'Yearly Statistics':
         return False
@@ -69,8 +66,8 @@ def update_input_container(selected_statistics):
 
 @app.callback(
     Output(component_id='output-container', component_property='children'),
-    [Input(component_id='select-year', component_property='value'), Input(component_id='dropdown-statistics', component_property='value')])
-
+    [Input(component_id='select-year', component_property='value'), Input(component_id='dropdown-statistics', component_property='value')]
+)
 def update_output_container(selected_year, selected_statistics):
     if selected_statistics == 'Yearly Statistics':
         return yearly_report(selected_year)
@@ -82,26 +79,28 @@ def yearly_report(selected_year):
     return
 
 def compute_yearly_info(yearly_data):
-     return
+    return
  
 def recession_report():
     df = compute_recession_info(data[data['Recession'] == 1])
     yearly_rec, avg_sales_type, total_exp, unemployed_sales = df
-    chart1 = dcc.Graph(figure=px.line(yearly_rec, x='Year',y='Automobile_Sales', title="Automobile Sales Over Time"))
-    chart2 = dcc.Graph(figure=px.bar(avg_sales_type, x='Vehicle_Type', y='Automobile_Sales', title="Automobile Sales by Vehicle Type"))
-    chart3 = dcc.Graph(figure=px.pie(total_exp, names='Vehicle_Type', values='Advertising_Expenditure', title="Advertising Expenditure by Vehicle Type"))
-    chart4 = dcc.Graph(figure=px.bar(unemployed_sales, x='unemployment_rate', y='Automobile_Sales', title="Unemployment Rate Effect on Automobile Sales by Vehicle Type"))
+    
+    # Apply dark theme to charts and customize chart colors
+    chart1 = dcc.Graph(figure=px.line(yearly_rec, x='Year', y='Automobile_Sales', title="Automobile Sales Over Time").update_layout(template="plotly_dark"), style={'width': '100%'})
+    chart2 = dcc.Graph(figure=px.bar(avg_sales_type, x='Vehicle_Type', y='Automobile_Sales', title="Automobile Sales by Vehicle Type", color='Vehicle_Type').update_layout(template="plotly_dark"), style={'width': '100%'})
+    chart3 = dcc.Graph(figure=px.pie(total_exp, names='Vehicle_Type', values='Advertising_Expenditure', title="Advertising Expenditure by Vehicle Type").update_layout(template="plotly_dark"), style={'width': '100%'})
+    chart4 = dcc.Graph(figure=px.bar(unemployed_sales, x='unemployment_rate', y='Automobile_Sales', title="Unemployment Rate Effect on Automobile Sales by Vehicle Type", color='Vehicle_Type').update_layout(template="plotly_dark"), style={'width': '100%'})
     
     return [
-            html.Div(className='chart-item', children=[html.Div(children=chart1), html.Div(children=chart2)]),
-            html.Div(className='chart-item', children=[html.Div(children=chart3), html.Div(children=chart4)]),
-            ]
+        html.Div(className='chart-item', children=[chart1, chart2], style={'flex': '50%'}),
+        html.Div(className='chart-item', children=[chart3, chart4], style={'flex': '50%'}),
+    ]
        
 def compute_recession_info(recession_data):
     yearly_rec = recession_data.groupby('Year')['Automobile_Sales'].mean().reset_index()
     avg_sales_type = recession_data.groupby('Vehicle_Type')['Automobile_Sales'].mean().reset_index()
     total_exp = recession_data.groupby('Vehicle_Type')['Advertising_Expenditure'].sum().reset_index()
-    unemployed_sales = recession_data.groupby(['unemployment_rate','Vehicle_Type'])['Automobile_Sales'].sum().reset_index()
+    unemployed_sales = recession_data.groupby(['unemployment_rate', 'Vehicle_Type'])['Automobile_Sales'].sum().reset_index()
     return yearly_rec, avg_sales_type, total_exp, unemployed_sales
     
 # Run the app
